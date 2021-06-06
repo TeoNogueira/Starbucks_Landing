362df107a0179f3644cb3aaac5396655f30a357e
Teo Nogueira <teonogueira17@gmail.com>
2021-06-05 21:00:30 -0300

    finish_Starbucks_

---
+++ assets/js/imgChange.js
@@ -0,0 +1,14 @@
+const addImage = (images) => {
+    document.querySelector('.starbucks').src = images;
+
+ } 
+ 
+ 
+ const changeCircleColor = (color) => {
+
+    document.querySelector('.circle').style.background = color
+
+
+ }
+
+
